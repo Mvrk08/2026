f2c208837b74cbc862f80f045ecd1341d27a9610
mrmahi2003 <maahirchauhan20@icloud.com>
2026-01-02 01:08:49 +0530

    2026

---
+++ week1.js
@@ -0,0 +1,32 @@
+let name = "maahir";
+const $sum =" x+y+z ";
+const x = 45;
+const y= 45;
+const z= 45;
+
+console.log( x+y+z);
+console.log(name);
+
+
+let num1 = "CristianoRonaldodos ";
+let char = num1.toUpperCase();
+
+function paaji() {
+  console.log("mvrk") ;
+}
+paaji()
+
+
+function add( a,b) {
+  a=24;
+  b=33;
+add =a+b;
+console.log(add) ;
+}
+add()
+
+function add( a,b) {
+  add =a+b;
+console.log(add) ;
+ }
+add(111 ,333)
