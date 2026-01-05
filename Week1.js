879723c732c5fa9279798852eec9460c04eecc66
mrmahi2003 <maahirchauhan20@icloud.com>
2026-01-06 01:06:52 +0530

    2026
    Signed-off-by: mrmahi2003 <maahirchauhan20@icloud.com>

---
+++ Week1.js
@@ -0,0 +1,76 @@
+const a = 29 ;
+ const  b =244; 
+ let result = a<b ;
+console.log(result) ;
+
+
+let num =-3234 ;
+if  (num>=90)
+{
+   console.log ("positive no") ;
+ }
+  else {
+    console.log("negative no ") ;
+  }
+
+
+let age =9 ;
+if(age>18) 
+{
+  console.log("nigga you are dead") ; 
+}
+else if  (age<=18) 
+{
+  console.log("my nigga is too high");
+}
+else (age<18)
+{
+  console.log("nigga u r a real nigga") ;
+}
+
+
+for(i = 1; i<=20; i++) 
+  if (i%2==0){
+  console.log(i) ;
+}
+
+const clothing = ["1", "2", "socks", "sweaters",
+                 
+                ] ;
+for (let i = 0; i < clothing.length; i++) {
+  console.log(clothing[i]);
+}
+
+
+function printNumbers(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+printNumbers(25);
+
+  let newone = "rahul";
+
+console.log(newone[0]); 
+console.log(newone.toUpperCase()); 
+console.log(newone.toLowerCase()); 
+console.log(newone.length); 
+console.log(newone.indexOf("4")); 
+console.log(newone.includes("one")); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
